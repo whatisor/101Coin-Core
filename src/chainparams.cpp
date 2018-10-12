7592b9bcfaf533015bfed4ba4bb1e15b718658c1
Guy Jordan <38801547+101-Coin@users.noreply.github.com>
2018-10-12 16:06:33 +0800

    Update src/chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -273,7 +273,7 @@ public:
         genesis.nNonce = 4192701;
 
         hashGenesisBlock = genesis.GetHash();
-
+		
         assert(hashGenesisBlock == uint256("00000050886fd54386bdb7ebcd3390ab222d5d81264e26249a59e978fb999ea7"));
 		assert(genesis.hashMerkleRoot == uint256("fdab1dc28f9886fd403dbd599af67e8e928ed4d02d540de6455c7d1670aa06cf"));
         vFixedSeeds.clear();
@@ -342,7 +342,30 @@ public:
         genesis.nNonce = 12345;
 
         hashGenesisBlock = genesis.GetHash();
-		   
+		 		 if(genesis.GetHash() != uint256("0x"))
+        {
+        printf("MSearching for genesis block...\n");
+        uint256 hashTarget;
+        hashTarget.SetCompact(genesis.nBits);
+        while(uint256(genesis.GetHash()) > uint256(hashTarget))
+        {
+            ++genesis.nNonce;
+            if (genesis.nNonce == 0)
+            {
+                printf("testnet NONCE WRAPPED, incrementing time");
+                std::cout << std::string("testnet NONCE WRAPPED, incrementing time:\n");
+                ++genesis.nTime;
+            }
+            if (genesis.nNonce % 10000 == 0)
+            {
+               printf("testnet: nonce %08u: hash = %s \n", genesis.nNonce, genesis.GetHash().ToString().c_str());
+            }
+        }
+        printf("testnet block.nTime = %u \n", genesis.nTime);
+        printf("testnet block.nNonce = %u \n", genesis.nNonce);
+        printf("testnet block.hashMerkleRoot: %s\n", genesis.hashMerkleRoot.ToString().c_str());
+        printf("testnet block.GetHash = %s\n", genesis.GetHash().ToString().c_str());
+        }
 		
         nDefaultPort = 51476;
 		
